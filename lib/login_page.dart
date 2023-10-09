6509793134be8fb54cfa27059abca2658b7baad8
Sara <sara.bastidas@upb.edu.co>
2023-10-09 10:26:02 -0500

    cambios en la pagina principal

--- lib/login_page.dart
+++ lib/login_page.dart
@@ -2,17 +2,95 @@ import 'dart:html';
 
 import 'package:flutter/material.dart';
 
-class LoginPage extends StatelessWidget{
-  const LoginPage({super.key});
+class LoginPage extends StatelessWidget {
+  const LoginPage({Key? key});
 
   @override
   Widget build(BuildContext context) {
-    return  Scaffold(
+    return Scaffold(
       backgroundColor: const Color(0xff53B3C1),
-    // body: Column(children: [
-      
-
-    // ]),
-     );  
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Text(
+              'ApartaApp',
+              style: TextStyle(
+                fontFamily: 'Inter', // Tipo de letra "Inter"
+                fontWeight: FontWeight.bold, // Negritas
+                fontSize: 100.0, // Tamaño de fuente reducido
+                color: Colors.white, // Texto blanco
+                letterSpacing: 10.0, // Espaciado personalizado entre letras
+                shadows: [
+                  Shadow(
+                    offset: Offset(2.0, 2.0), // Desplazamiento de la sombra
+                    blurRadius: 5.0, // Radio de desenfoque de la sombra
+                    color: Colors.black, // Color de la sombra
+                  ),
+                ],
+              ),
+            ),
+            const SizedBox(height: 20), // Espacio entre líneas de texto
+            Text(
+              'Ingresa tu ID',
+              style: TextStyle(
+                fontFamily: 'Inter', // Tipo de letra "Inter"
+                fontWeight: FontWeight.w600, // Negritas semi bold
+                fontSize: 32.0, // Tamaño de fuente reducido
+                color: Colors.white, // Texto blanco
+              ),
+            ),
+            const SizedBox(height: 20), // Espacio entre líneas de texto y campo de texto
+            SizedBox(
+              width: 300, // Ancho del campo de texto
+              child: TextFormField(
+                style: TextStyle(
+                  fontSize: 18,
+                  color: Colors.white, // Texto blanco
+                ),
+                decoration: InputDecoration(
+                  hintText: 'Ingresa algo', // Utiliza hintText en lugar de labelText
+                  hintStyle: TextStyle(
+                    color: Colors.white, // Color del texto de sugerencia (hint)
+                  ),
+                  filled: true, // Relleno blanco
+                  fillColor: Colors.white, // Color del relleno
+                  enabledBorder: OutlineInputBorder(
+                    borderSide: BorderSide(
+                      color: Colors.white, // Bordes blancos
+                    ),
+                  ),
+                  focusedBorder: OutlineInputBorder(
+                    borderSide: BorderSide(
+                      color: Colors.white, // Bordes blancos cuando está enfocado
+                    ),
+                  ),
+                ),
+              ),
+            ),
+            const SizedBox(height: 20), // Espacio entre campo de texto y botón
+            ElevatedButton(
+              onPressed: () {
+                // Lógica del botón
+              },
+              child: Text(
+                'Iniciar Sesión',
+                style: TextStyle(
+                  fontFamily: 'Inter', // Tipo de letra "Inter"
+                  fontWeight: FontWeight.w600, // Semibold
+                  fontSize: 25.0, // Tamaño de fuente 40
+                ),
+              ),
+              style: ElevatedButton.styleFrom(
+                primary: const Color(0xff2986AE), // Cambio de color del botón
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
+
+
---
+++ lib/pagina2.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'package:proyecto1/main.dart';
+
+class Pagina2 extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Página 2'),
+      ),
+      body: Center(
+        child: Text('Contenido de la Página 2'),
+      ),
+    );
+  }
+}
